d396a320c48be5d6c1e44d8f33dd25eadbcd46de
jmlorenzo <juanmanuel.lorenzolopez@gmail.com>
2021-05-28 21:46:07 +0200

    Access Modifiers

---
+++ Access_Modifiers/Example5/Example52/Example52/Program.cs
@@ -0,0 +1,27 @@
+﻿using Example41;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example42
+{
+    //Second project (ASSEMBLY)
+    class Program : NumberClassInFirstProject //Inheritance
+    {
+        public void Print()
+        {
+            Console.WriteLine(number); //This is OK as well. The class Program derives from the NumberClassInFirstProject clas.
+        }
+    }
+
+    class Program1
+    {
+        static void Main()
+        {
+            Program program = new Program();
+            program.Print();
+        }
+    }
+}
