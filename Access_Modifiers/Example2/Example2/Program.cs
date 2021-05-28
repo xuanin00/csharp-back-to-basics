9fe9b8ec25b218d70a24540d27ad232c1e7443b4
jmlorenzo <juanmanuel.lorenzolopez@gmail.com>
2021-05-28 06:49:57 +0200

    Access Modifiers

---
+++ Access_Modifiers/Example2/Example2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    class NumberClass
+    {
+        public int number = 10;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberClass num = new NumberClass();
+            Console.WriteLine(num.number); // Error. We can't access the number variable because 
+                                           // it has the private access modifier and its only accessible in the NumberClass class
+        }
+    }
+}
